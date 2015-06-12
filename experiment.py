1fd1bfd9efb8f5e6afb2b0a0a53f21b1b2f291ff
Yanir Seroussi <yanir.seroussi@gmail.com>
2015-06-12 15:37:06 +1000

    improved experiment reporting, added command-line setting of learning rate and break on divergence

--- experiment.py
+++ experiment.py
@@ -3,6 +3,8 @@ from time import time
 import sys
 
 from commandr import command
+import lasagne
+import numpy as np
 import theano
 from theano_latest.misc import pkl_utils
 
@@ -11,14 +13,14 @@ from architectures import ARCHITECTURE_NAME_TO_CLASS
 
 @command
 def run_experiment(dataset_path, model_architecture, model_params=None, num_epochs=500, batch_size=100,
-                   training_chunk_size=0, reshape_to=None):
+                   training_chunk_size=0, reshape_to=None, learning_rate=0.01):
     """Run a deep learning experiment, reporting results to standard output.
 
     Command line or in-process arguments:
      * dataset_path (str) - path of dataset pickle zip (see data.create_datasets)
      * model_architecture (str) - the name of the architecture to use (subclass of architectures.AbstractModelBuilder)
-     * model_params (str) - comma separated list of key-value pairs to pass to the model builder. All keys are assumed
-                            to be strings, while values are evaluated as Python literals
+     * model_params (str) - colon-separated list of equals-separated key-value pairs to pass to the model builder.
+                            All keys are assumed to be strings, while values are evaluated as Python literals
      * num_epochs (int) - number of training epochs to run
      * batch_size (int) - number of examples to feed to the network in each batch
      * training_chunk_size (int) - number of training examples to copy to the GPU in each chunk. If set to zero, all
@@ -26,6 +28,7 @@ def run_experiment(dataset_path, model_architecture, model_params=None, num_epoc
                                    set is larger than the GPU's memory
      * reshape_to (str) - if given, the data will be reshaped to match this string, which should evaluate to a Python
                           tuple of ints (e.g., may be required to make the dataset fit into a convnet input layer)
+     * learning_rate (float) - learning rate to use for training the network
     """
     assert theano.config.floatX == 'float32', 'Theano floatX must be float32 to ensure consistency with pickled dataset'
     if not model_architecture in ARCHITECTURE_NAME_TO_CLASS:
@@ -34,9 +37,11 @@ def run_experiment(dataset_path, model_architecture, model_params=None, num_epoc
 
     dataset, label_to_index = _load_data(dataset_path, reshape_to)
     model_builder = ARCHITECTURE_NAME_TO_CLASS[model_architecture](
-        dataset, output_dim=len(label_to_index), batch_size=batch_size, training_chunk_size=training_chunk_size
+        dataset, output_dim=len(label_to_index), batch_size=batch_size, training_chunk_size=training_chunk_size,
+        learning_rate=learning_rate
     )
-    _, training_iter, validation_eval = model_builder.build(**_parse_model_params(model_params))
+    output_layer, training_iter, validation_eval = model_builder.build(**_parse_model_params(model_params))
+    print('Network has {:,} parameters'.format(lasagne.layers.count_params(output_layer)))
     _run_training_loop(training_iter, validation_eval, num_epochs)
 
 
@@ -53,12 +58,13 @@ def _load_data(dataset_path, reshape_to):
 def _parse_model_params(model_params):
     param_kwargs = {}
     if model_params:
-        for pair in model_params.split(','):
+        for pair in model_params.split(':'):
             key, value = pair.split('=')
             try:
                 param_kwargs[key] = literal_eval(value)
             except (SyntaxError, ValueError):
                 param_kwargs[key] = value
+        print('Parsed model params: {}'.format(param_kwargs))
     return param_kwargs
 
 
@@ -76,5 +82,9 @@ def _run_training_loop(training_iter, validation_eval, num_epochs):
             print('\ttraining loss:\t\t\t %.6f' % training_loss)
             print('\tvalidation loss & accuracy:\t %.6f\t%.2f%%' % (validation_loss, validation_accuracy * 100))
             sys.stdout.flush()
+
+            if np.isnan(training_loss) or np.isnan(validation_loss) or np.isnan(validation_accuracy):
+                print('Divergence detected. Stopping now.')
+                break
     except KeyboardInterrupt:
         pass
