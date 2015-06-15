810d8ccf00d48c0d10a451b3a00127cf7bfc6707
Yanir Seroussi <yanir.seroussi@gmail.com>
2015-06-15 16:21:58 +1000

    implemented vggnet's architecture

--- architectures.py
+++ architectures.py
@@ -38,7 +38,7 @@ class ConvNet(AbstractModelBuilder):
         for i in xrange(num_conv_layers):
             conv_kwargs = self._extract_layer_kwargs('c', i, kwargs)
             if 'border_mode' not in conv_kwargs:
-                conv_kwargs['border_mode'] = 'full'
+                conv_kwargs['border_mode'] = 'same'
             has_max_pool = conv_kwargs.pop('mp', False)
             l_bottom = Conv2DLayer(l_bottom, **conv_kwargs)
 
@@ -55,7 +55,7 @@ class ConvNet(AbstractModelBuilder):
 
     def _extract_layer_kwargs(self, layer_letter, layer_index, kwargs):
         return {k.split('_', 1)[1]: v for k, v in kwargs.iteritems()
-                if k.startswith('l%s%d' % (layer_letter, layer_index))}
+                if k.startswith('l%s%d_' % (layer_letter, layer_index))}
 
 
 class LasagneMnistConvExample(ConvNet):
@@ -72,6 +72,36 @@ class LasagneMnistConvExample(ConvNet):
             lc1_num_filters=32, lc1_filter_size=(5, 5),
             ld0_num_units=256
         )
+
+
+class VggNet(ConvNet):
+    """Network D by VGGNet, from Very deep convolutional networks for large scale image recognition
+    (Simonyan and Zisserman, 2014). Includes an optional reduction factor to reduce the number of filters and hidden
+    units.
+    """
+    def _build_middle(self, l_in, reduction_factor=1, **_):
+        kwargs = dict(
+            num_conv_layers=13, num_dense_layers=2,
+            lc0_num_filters=64, lc0_filter_size=3,
+            lc1_num_filters=64, lc1_filter_size=3, lc1_mp=True,
+            lc2_num_filters=128, lc2_filter_size=3,
+            lc3_num_filters=128, lc3_filter_size=3, lc3_mp=True,
+            lc4_num_filters=256, lc4_filter_size=3,
+            lc5_num_filters=256, lc5_filter_size=3,
+            lc6_num_filters=256, lc6_filter_size=3, lc6_mp=True,
+            lc7_num_filters=512, lc7_filter_size=3,
+            lc8_num_filters=512, lc8_filter_size=3,
+            lc9_num_filters=512, lc9_filter_size=3, lc9_mp=True,
+            lc10_num_filters=512, lc10_filter_size=3,
+            lc11_num_filters=512, lc11_filter_size=3,
+            lc12_num_filters=512, lc12_filter_size=3, lc12_mp=True,
+            ld0_num_units=4096,
+            ld1_num_units=4096
+        )
+        for key, value in kwargs.iteritems():
+            if key.endswith('num_filters') or key.endswith('num_units'):
+                kwargs[key] = value / reduction_factor
+        return super(VggNet, self)._build_middle(l_in, **kwargs)
 
 
 def _build_dense_plus_dropout(incoming_layer, num_units):
